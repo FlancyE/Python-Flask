24d13f0333fdf742274e199ae6d6fd988043278e
FlancyE <132045261+FlancyE@users.noreply.github.com>
2024-01-26 03:10:05 +0000

    update

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app=Flask(__name__)     #建立 Application 物件
 def index():    #用來回應網站首頁連線的函式
     return "Hello Flask"    #回傳網站首頁的內容
 
+@app.route("/data")
+def handleData():
+    return "My data"
+
+@app.route("/user/<username>")
+def handleUser(username):
+    if username == "Flancye":
+        return "你好"+ username
+    else:
+        return "Hello"+username
 #啟動網站伺服器
 app.run(port=3000)  # 可以使用 port 參數去指定埠號
